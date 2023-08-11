1b8cba7969067f7851d49280217c66fc702ab9d0
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-11 21:37:11 +0200

    Update python_init_pract.py

--- Playground/python_init_pract.py
+++ Playground/python_init_pract.py
@@ -25,18 +25,19 @@ class Stats:
         b = len(self.list)
         return int(a / b)
 
-list_2 = Stats([100, 90, 150, 250, 200])
-
-rand_iter = random.randint(1, 10)
-rand_10_num_list = [random.randint(0, 2000) for _ in range(rand_iter)]
-class_rand = Stats(rand_10_num_list)
+#list_2 = Stats([100, 90, 150, 250, 200])
 #print(list_2.median())
 #print(list_2.min(), list_2.max(), list_2.mean())
-print(f'''\nThe list including random number of random elements is below: {rand_10_num_list}
-Some stats of the provided data are here as follows:
+
+rand_iter = int(input('Please enter the size of a list which will generate random numbers and make some stats of that:'))
+rand_10_num_list = [random.randint(0, 2000) for _ in range(rand_iter)]
+class_rand = Stats(rand_10_num_list)
+
+print(f'''\nThe list including {rand_iter} random elements is below: {rand_10_num_list}
+Some basic stats of the provided data are here as follows:
 
 - min: {class_rand.min()}
 - max: {class_rand.max()}
 - median: {class_rand.median()}
-- mean: {class_rand.mean()}''')  
+- mean: {class_rand.mean()}\n''')
     
