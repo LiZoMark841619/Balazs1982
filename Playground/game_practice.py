b41e4071047c431bf1ef745a4c8a1ad63887835b
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-11 22:27:43 +0200

    Update game_practice.py

--- Playground/game_practice.py
+++ Playground/game_practice.py
@@ -6,10 +6,9 @@ class Game:
         self.pers_2 = pers_2
         
     def __repr__(self):
-        return f'Your choosen character is {gamer}.\nYour enemy is {enemy}.\nYou can have a duel now.\n{Game.damage(self)}'
+        return f'Your choosen character is {gamer}.\nYour randomly picked enemy is {enemy}.\nYou can have a duel now.\n{Game.damage(self)}'
         
     def damage(self):
-
         if (self.pers_1 or self.pers_2) in ['Superman', 'Wanda', 'Thor', 'Hulk']:
             self.dam1 = random.randint(50, 100)
             self.dam2 = random.randint(50, 100)
@@ -17,6 +16,7 @@ class Game:
             self.dam1 = random.randint(10, 20)
             self.dam2 = random.randint(10, 20)
         result = self.dam1 - self.dam2
+        
         str1 = ['You won ', 'You lost ', 'It is a draw, neither of you won the game, please try again!']
         str2 = f'because you damaged {self.dam1} and your enemy {self.dam2}'
         if result > 0:
@@ -34,10 +34,9 @@ enemy_chars = [name for name in gamer_chars if name !=gamer]
 enemy = random.choice(enemy_chars)
 
 game1 = Game(gamer, enemy)
-print(game1)
-#print(game1.__repr__())
+#Some testing
 #print(game1.damage())
-
+print(game1)
     
     
         
