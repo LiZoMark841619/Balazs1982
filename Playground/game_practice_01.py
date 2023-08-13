1c0d7fd0594d377e33bcb08d283b8c4d1a25e2e3
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-13 20:57:03 +0200

    Update game_practice_01.py

--- Playground/game_practice_01.py
+++ Playground/game_practice_01.py
@@ -1,45 +1,54 @@
-import time
-import random
+import random as rm
+#Marvel vs DC Universe
 
-gamer_chars = ['Hulk', 'Thor', 'Wanda', 'Iron man']
-gamer = input(f'Please pick a character from this list {gamer_chars}:')
-enemy_chars = [name for name in gamer_chars if name !=gamer]
-enemy = random.choice(enemy_chars)
+gamer_chars = ['Hulk', 'Thor', 'Iron man', 'Superman', 'Black Adam', 'Batman']
+gamer = input(f'Please pick your character from this list {gamer_chars}:')
+enemy_chars = [name for name in gamer_chars if name != gamer]
+enemy = input(f'Please pick your enemy from this list {enemy_chars}:')
 
 class Character:
-    characters = {'Hulk' : {'power':random.randint(100, 2000), 'speed':500, 'life': 200},
-                  'Thor' : {'power':random.randint(100, 3000), 'speed':1000, 'life': 2000},
-                  'Wanda' : {'power':random.randint(200, 4500), 'speed':250, 'life': 100},
-                  'Iron man' : {'power':500, 'speed':2000, 'life': 100},
+    
+    a = rm.randint(100, 2000)
+    b = rm.randint(200, 4000)
+    c = rm.randint(10, 500)
+    d = rm.randint(50, 2500)
+    
+    characters = {'Hulk' : {'power':b, 'speed':c, 'life': 100},
+                  'Thor' : {'power':b, 'speed':d, 'life': 3000},
+                  'Iron man' : {'power':a, 'speed':d, 'life': 100},
+                  'Superman' : {'power':b, 'speed':d, 'life': 100},
+                  'Black Adam' : {'power':b, 'speed':d, 'life': 3000},
+                  'Batman' : {'power':a, 'speed':c, 'life': 100}
                   }
+    
     gamers_count = 0
           
     def __init__(self, name):
-        
         self.name = name
         self.attributes()
         type(self).gamers_count +=1
         
     def __repr__(self):
-        return f'Your choosen character is {self.name}.\nYour randomly picked enemy is {enemy}.\nYou can have a duel now. '
+        return f'Your choosen character is {self.name}.'
     
     def attributes(self):
         self.power = type(self).characters[self.name]['power']
         self.speed = type(self).characters[self.name]['speed']
         self.life = type(self).characters[self.name]['life']
         return f'Your character power is {self.power}, your speed is {self.speed}, your life value is {self.life}'
-    
 
-game1 = Character(gamer)
-game2 = Character(gamer)
-game3 = Character(gamer)
-game4 = Character(gamer)
+#Test
+gamer1 = Character(gamer)
+gamer2 = Character('Thor')
+gamer3 = Character('Iron man')
 
-print(game1)
-print(game1.attributes())
+print(gamer1)
+print(gamer1.attributes())
 print(f'We got {Character.gamers_count} players registered')
 
 class Game:
     def __init__(self, gamer, enemy):
-        self.Character.name = gamer
+        self.gamer = gamer
         self.enemy = enemy
+        
+    def damage(self):pass
