92f5afc42fccc522d64467e4f9f7091fc25f338b
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-13 19:20:12 +0200

    Update game_practice_01.py

--- Playground/game_practice_01.py
+++ Playground/game_practice_01.py
@@ -7,8 +7,11 @@ enemy_chars = [name for name in gamer_chars if name !=gamer]
 enemy = random.choice(enemy_chars)
 
 class Character:
-    characters = {'Hulk' : {'power':1000, 'speed':500, 'life': 200}, 'Thor' : {'power':1250, 'speed':1000, 'life': 2000},
-                  'Wanda' : {'power':2000, 'speed':250, 'life': 100}, 'Iron man' : {'power':500, 'speed':2000, 'life': 100},}
+    characters = {'Hulk' : {'power':random.randint(100, 2000), 'speed':500, 'life': 200},
+                  'Thor' : {'power':random.randint(100, 3000), 'speed':1000, 'life': 2000},
+                  'Wanda' : {'power':random.randint(200, 4500), 'speed':250, 'life': 100},
+                  'Iron man' : {'power':500, 'speed':2000, 'life': 100},
+                  }
     gamers_count = 0
           
     def __init__(self, name):
@@ -16,6 +19,9 @@ class Character:
         self.name = name
         self.attributes()
         type(self).gamers_count +=1
+        
+    def __repr__(self):
+        return f'Your choosen character is {self.name}.\nYour randomly picked enemy is {enemy}.\nYou can have a duel now. '
     
     def attributes(self):
         self.power = type(self).characters[self.name]['power']
@@ -23,8 +29,6 @@ class Character:
         self.life = type(self).characters[self.name]['life']
         return f'Your character power is {self.power}, your speed is {self.speed}, your life value is {self.life}'
     
-    def __repr__(self):
-        return f'Your choosen character is {self.name}.\nYour randomly picked enemy is {enemy}.\nYou can have a duel now. '
 
 game1 = Character(gamer)
 game2 = Character(gamer)
@@ -34,4 +38,8 @@ game4 = Character(gamer)
 print(game1)
 print(game1.attributes())
 print(f'We got {Character.gamers_count} players registered')
-print(game2)
+
+class Game:
+    def __init__(self, gamer, enemy):
+        self.Character.name = gamer
+        self.enemy = enemy
