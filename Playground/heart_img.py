88951c74f83d3ed3341a98ad3281c97a4d8fb791
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-16 18:58:29 +0200

    Update heart_img.py

--- Playground/heart_img.py
+++ Playground/heart_img.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import codecademylib3
+from PIL import Image
 
 heart_img = np.array([[255,0,0,255,0,0,255],
               [0,255/2,255/2,0,255/2,255/2,0],
@@ -11,6 +13,34 @@ heart_img = np.array([[255,0,0,255,0,0,255],
 
 # This is a helper function that makes it easy for you to show images!
 def show_image(image, name_identifier):
-  plt.imshow(image, cmap="gray")
+  plt.imshow(image, cmap="pink")
   plt.title(name_identifier)
   plt.show()
+
+# Show heart image
+show_image(heart_img, 'Heart Image')
+
+# Invert color
+inverted_heart_img = 255 - heart_img
+show_image(inverted_heart_img, 'Inverted Heart Image.')
+
+# Rotate heart
+rotated_heart_img = heart_img.T
+show_image(rotated_heart_img, 'Rotated Heart Image.')
+
+# Random Image
+random_img = np.random.randint(0,255,(7,7))
+show_image(random_img, 'Random Image.')
+
+# Solve for heart image
+x = heart_img / random_img
+show_image(x, 'x.')
+solved_heart_image = random_img * x
+show_image(solved_heart_image, 'Solved Heart Image.')
+
+new_img= Image.open("pict.jpg")
+np_array_img = np.array(new_img)
+show_image(np_array_img, 'Sun')
+
+print(np_array_img.shape)
+
