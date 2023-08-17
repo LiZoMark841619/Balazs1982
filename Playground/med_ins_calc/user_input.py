85a2b0df8b8f112d024822899da796092b8bc5db
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-17 12:52:53 +0200

    accepted

--- Playground/med_ins_calc/user_input.py
+++ Playground/med_ins_calc/user_input.py
@@ -1,6 +1,7 @@
 from insurance_classes import *
 
-# Making objects by using for loop
+# Making objects by making an empty list and add the persons/patients which are about to be created/registered.
+
 persons = []
 for i in range(int(input('Please enter the number of patients to be registered:'))):
     person = Patient(input(f"Enter the number {i+1} patient's name:"), int(input('His/her age in years:')),
@@ -10,17 +11,20 @@ for i in range(int(input('Please enter the number of patients to be registered:'
     persons.append(person)
     
 # Traversing persons and print every patient information and insurance cost by using patient_profile and estimated_insurace_cost methods.
+
 for person in persons:
     print(f"\nPatient {person.name}'s profile information is below: {person.patient_profile()}")
-    print(f"The estimated insurance cost is {person.estimated_insurance_cost()} dollars.\n")
+    print(f"The estimated insurance cost is {int(person.estimated_insurance_cost()):,} dollars.\n")
 if Patient.counter == 1:
     print(f'\nYou registered {Patient.counter} patient successfully.\n')
 else:
     print(f'\nYou registered {Patient.counter} patients successfully.\n')
 
 # Selecting one patient to use the update_age and estimated_insurance cost method to see "Inheritence".
-requested_name = input("Enter the patient's name whose age must be updated:\n")
+
+req_pers_setage = input("Enter the patient's name whose age must be updated:\n")
 for person in persons:
-    if requested_name == person.name:
-        print(f"{person.name}'s updated age is {person.update_age(int(input('Enter current age:')), force=True)}.")
-        print(f"The estimated insurance cost is {person.estimated_insurance_cost()} dollars.")
+    if req_pers_setage == person.name:
+        updated_age = person.update_age(int(input('Enter current age:')), force =True)
+        print(f"{person.name} is {updated_age} years old now.")
+        print(f"The modified estimated insurance cost is {int(person.estimated_insurance_cost()):,} dollars.")
