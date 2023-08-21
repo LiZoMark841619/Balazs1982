f270a37c772f3effd66ed27a26d28399ffedb8bf
LiZoMark841619 <balazsarva1982@gmail.com>
2023-08-21 12:56:40 +0200

    Update user_input.py

--- Playground/Practice_pro/med_ins_calc/user_input.py
+++ Playground/Practice_pro/med_ins_calc/user_input.py
@@ -2,14 +2,11 @@ from insurance_classes import *
 
 # Making objects by making an empty list and add the persons/patients which are about to be created/registered.
 
-persons = []
-for i in range(int(input('Please enter the number of patients to be registered:'))):
-    person = Patient(input(f"Enter the number {i+1} patient's name:"), int(input('His/her age in years:')),
-    int(input('Sex code (0 = male, 1 = female):')),
-    float(input('BMI in decimals:')), int(input('Number of children:')),
-    int(input('Smoker status code (0 = non smoker, 1 = smoker):')))
-    persons.append(person)
-    
+persons = [Patient(input(f"Enter the number {i+1} patient's name:"), int(input('His/her age in years:')),
+    int(input('Sex code (0 = male, 1 = female):')), float(input('BMI in decimals:')), int(input('Number of children:')),
+    int(input('Smoker status code (0 = non smoker, 1 = smoker):'))) 
+    for i in range(int(input('Please enter the number of patients to be registered:')))]
+
 # Traversing persons and print every patient information and insurance cost by using patient_profile and estimated_insurace_cost methods.
 
 for person in persons:
