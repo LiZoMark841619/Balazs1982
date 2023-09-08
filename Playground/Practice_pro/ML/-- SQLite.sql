5f5080eea67ba6f2d1c46288b3c822d78e7a7f1c
LiZoMark841619 <balazsarva1982@gmail.com>
2023-09-08 15:19:11 +0200

    accepted

--- Playground/Practice_pro/ML/-- SQLite.sql
+++ Playground/Practice_pro/ML/-- SQLite.sql
@@ -3,3 +3,21 @@ SELECT name, total_population, per_capita_income FROM states
 WHERE median_age > 35 AND total_households > 50000
 GROUP BY name
 ORDER BY total_population DESC;
+
+-- SELECT * FROM family;
+
+-- SELECT name, num_child FROM family WHERE num_child !=0;
+
+SELECT name, SUM(age) AS age
+FROM family 
+GROUP BY name 
+HAVING age > 20;
+
+SELECT name, total_population AS total_pop,
+white, black, hispanic, asian, american_indian AS indian,
+pacific_islander AS pacific, other_race AS other,
+median_age, total_households,
+owner_occupied_homes_median_value AS homes_median_value, per_capita_income
+FROM states;
+
+
