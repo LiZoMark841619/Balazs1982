df4500c66f554fe9567e03237e88b71e5d0fc57e
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-08 17:11:23 +0100

    ok

--- teszt_program_odootech_zrt/docstring.py
+++ teszt_program_odootech_zrt/docstring.py
@@ -5,12 +5,24 @@ import pickle
 from pathlib import Path 
 
 
-def content(parent=Path.cwd()):
+def content(parent='current working directory'):
     
-    '''This method does not require a parent directory as attribute, because its default directory is the current working directory.
-    The default value is a method which returns to the current WindowsPath directly, and the result of the content method is a return value of
-    one dictionary with directories as keys and every subfolder and file as values'''
+    '''
+    This method does not require a parent directory as attribute, because its default directory is the current working directory.
+    The default value is a method which returns to the current WindowsPath directly, and the result of the content method is 
+    a return value of one dictionary with directories as keys and every subfolder and file as values.
+    '''
     
     return {root:(dir, file) for root, dir, file in os.walk(top=parent, topdown=False)}
+
+def read_files(dirs: dict) -> list:
+    
+    '''
+    Practicing
+    '''
+    for key in dirs:
+        for file in dirs[key][1]:
+            if '.dat' in file:
+                yield open(key+'\\'+file).read()
 
 pydoc.writedoc('docstring')
