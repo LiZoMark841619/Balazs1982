7fe3fa7c38e71f86f216fea9a87add5d52d51a22
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-08 09:23:49 +0100

    ok

---
+++ teszt_program_odootech_zrt/docstring.py
@@ -0,0 +1,10 @@
+import pydoc
+
+'''
+This is a folder-file searching code in which you can load in every data from every file. Moreover you can
+make auto and bicikli objects by increasing the members of some fleet or picking new instances randomly. 
+Then it will be saved in pickle file for later use. 
+'''
+
+pydoc.writedoc('my_module')
+    
