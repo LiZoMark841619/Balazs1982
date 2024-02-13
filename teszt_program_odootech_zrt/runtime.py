c08601d3338c70924826f560b906295b8d407f24
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-13 16:31:32 +0100

    accepted

---
+++ teszt_program_odootech_zrt/runtime.py
@@ -0,0 +1,8 @@
+import timeit
+repslist = range(1000)
+def timer(func, *pargs, **kargs):
+    start = timeit.timeit()
+    for _ in repslist:
+        ret = func(*pargs, **kargs)
+        elapsed = timeit.timeit() - start
+    return (elapsed, ret)
