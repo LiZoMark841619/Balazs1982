93295934876e50f2607e3a8c2e0cb802510e6d14
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-13 20:26:00 +0100

    ok

--- teszt_program_odootech_zrt/runtime.py
+++ teszt_program_odootech_zrt/runtime.py
@@ -1,8 +1,26 @@
-import time
-repslist = range(1000)
-def timer(func, *pargs, **kargs):
-    start = time.time()
-    for _ in repslist:
+# import time
+# repslist = range(1000)
+# def timer(func, *pargs, **kargs):
+#     start = time.time()
+#     for _ in repslist:
+#         ret = func(*pargs, **kargs)
+#     elapsed = time.time() - start
+#     return (elapsed, ret)
+
+import time, sys
+trace = lambda *args: None # or print
+timefunc = time.time
+def timer(func, *pargs, _reps=1000, **kargs):
+    trace(func, pargs, kargs, _reps)
+    start = timefunc()
+    for _ in range(_reps):
         ret = func(*pargs, **kargs)
-    elapsed = time.time() - start
+    elapsed = timefunc() - start
     return (elapsed, ret)
+def best(func, *pargs, _reps=50, **kargs):
+    best = 2 ** 32
+    for _ in range(_reps):
+        (time, ret) = timer(func, *pargs, _reps=1, **kargs)
+        if time < best: best = time
+    return (best, ret)
+
