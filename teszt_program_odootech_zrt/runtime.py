357da0f0e754ffee32443717d4550865a6c3c399
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-13 16:38:56 +0100

    ok

--- teszt_program_odootech_zrt/runtime.py
+++ teszt_program_odootech_zrt/runtime.py
@@ -1,8 +1,8 @@
 import timeit
-repslist = range(100)
+repslist = range(1000)
 def timer(func, *pargs, **kargs):
     start = timeit.timeit()
     for _ in repslist:
         ret = func(*pargs, **kargs)
-        elapsed = timeit.timeit() - start
+    elapsed = timeit.timeit() - start
     return elapsed
