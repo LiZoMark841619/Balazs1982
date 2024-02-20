471f635514e1ad977dce4ae95697163fb66c9f6b
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-20 01:24:49 +0100

    ok

--- pkg/spam.py
+++ pkg/spam.py
@@ -1,3 +1,3 @@
-# import string - this will skip search in the package and start making absolute import from Python lib.
+# import string # this will skip search in the package and start making absolute import from Python lib.
 from . import string # this however will search in the package meaning this is a relative import.
 print(string)
