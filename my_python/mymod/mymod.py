b50c26273b1e8eb65ca023ed6140fd6dd7558470
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-22 17:15:19 +0100

    ok

--- my_python/mymod/mymod.py
+++ my_python/mymod/mymod.py
@@ -8,13 +8,10 @@ def countChars(file):
     
 def test(file):
     return {'Lines':countLines(file), 'Chars':countChars(file)}
-if __name__=='__main__':
-    print(test('mymod.py'))
+if __name__=='__main__': print(test('mymod.py'))
     
 def countLinesChars(file):
     with open(file, encoding='utf') as F:
         L = F.readlines()
-        D = {i+1:len(L[i]) for i in range(len(L))}
-        return {'file': file.split('\\')[-1], 'Line:chars':D}
-if __name__=='__main__':
-    print(countLinesChars('mymod.py'))
+        return {'file': file.split('\\')[-1], 'Line:chars':{i+1:len(L[i]) for i in range(len(L))}}
+if __name__=='__main__': print(countLinesChars('mymod.py'))
