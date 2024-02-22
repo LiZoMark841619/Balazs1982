933e2f652d2b6e548027951df7b9ab2018d0637b
LiZoMark841619 <balazsarva1982@gmail.com>
2024-02-22 12:52:40 +0100

    ok

--- my_python/mymod/mymod.py
+++ my_python/mymod/mymod.py
@@ -1,25 +1,20 @@
 def countLines(file):
     with open(file, encoding='utf') as F:
         return len(F.readlines())
-print(countLines('..\\..\\teszt_program_odootech_zrt\\feladat.txt'))
-
+    
 def countChars(file):
     with open(file, encoding='utf') as F:
         return len(F.read())
-print(countChars('..\\..\\teszt_program_odootech_zrt\\feladat.txt'))
-
+    
 def test(file):
     return {'Lines':countLines(file), 'Chars':countChars(file)}
-print(test('..\\..\\teszt_program_odootech_zrt\\feladat.txt'))
+if __name__=='__main__':
+    print(test('mymod.py'))
     
-
-# def countLinesChars(path: str):
-#     with open(path, encoding='utf') as F:
-#         L = F.readlines()
-#         D = {i:len(L[i]) for i in range(len(L))}
-#         return path.split('\\')[-1], len(L), D
-    
-# test = countLinesChars('..\\..\\teszt_program_odootech_zrt\\feladat.txt')
-# print(f'The {test[0]} file has {test[1]} lines which contains characters as follows: ')
-# for k in test[-1]:
-#     print(f'Line number {k+1} has => {test[-1][k]} characters.')
+def countLinesChars(file):
+    with open(file, encoding='utf') as F:
+        L = F.readlines()
+        D = {i:len(L[i]) for i in range(len(L))}
+        return {'file': file.split('\\')[-1], 'Lines': len(L), 'Chars':D}
+if __name__=='__main__':
+    print(countLinesChars('mymod.py'))
