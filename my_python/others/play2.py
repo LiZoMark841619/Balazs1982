d418d0fe9f781cf6d03ab60e33a343ccae4d43f6
LiZoMark841619 <githubs001@gmail.com>
2024-03-25 17:17:43 +0100

    ok

--- my_python/others/play2.py
+++ my_python/others/play2.py
@@ -1,6 +1,8 @@
 class Adder:
   
-    def __init__(self, start=[]):
+    def __init__(self, start=None):
+        if start is None:
+            start = []
         self.data = start
         
     def __add__(self, other):
@@ -17,40 +19,6 @@ class DictAdder(Adder):
     def add(self, y):
         return {k:self.data[k] for k in self.data} | {k:y[k] for k in y}
 
-class Mylist:
-    pass
-class MyList:
-    def __init__(self, start):
-    #self.wrapped = start[:] # Copy start: no side effects
-        self.wrapped = [] # Make sure it's a list here
-        for x in start: self.wrapped.append(x)
-    def __add__(self, other):
-        return MyList(self.wrapped + other)
-    def __mul__(self, time):
-        return MyList(self.wrapped * time)
-    def __getitem__(self, offset):
-        return self.wrapped[offset]
-    def __len__(self):
-        return len(self.wrapped)
-    def __getslice__(self, low, high):
-        return MyList(self.wrapped[low:high])
-    def append(self, node):
-        self.wrapped.append(node)
-    def __getattr__(self, name): # Other methods: sort/reverse/etc
-        return getattr(self.wrapped, name)
-    def __repr__(self):
-        return repr(self.wrapped)
-if __name__ == '__main__':
-    x = MyList('spam')
-    print(x)
-    print(x[2])
-    print(x[1:])
-    print(x + ['eggs'])
-    print(x * 3)
-    x.append('a')
-    x.sort()
-    for c in x: print(c, end=' ')
-    
 if __name__ == '__main__':
     Adder(10).add(20)
     Adder('Hallo') + 'Balazs'
@@ -65,3 +33,68 @@ if __name__ == '__main__':
     E = D + {i-96:chr(i) for i in range(97, 97+26)}
     F = DictAdder(E) + {27:'end'}
     print(F)
+    
+class MyList:
+    def __init__(self, start):
+        self.wrapped = []
+        for x in start: self.wrapped.append(x)
+        
+    def __add__(self, other):
+        return MyList(self.wrapped + other)
+    
+    def __mul__(self, time):
+        return MyList(self.wrapped * time)
+    
+    def __getitem__(self, offset):
+        return self.wrapped[offset]
+    
+    def __len__(self):
+        return len(self.wrapped)
+    
+    def __getslice__(self, low, high):
+        return MyList(self.wrapped[low:high])
+    
+    def append(self, node):
+        self.wrapped.append(node)
+        
+    def __getattr__(self, name):
+        return getattr(self.wrapped, name)
+    
+    def __repr__(self):
+        return repr(self.wrapped)
+    
+if __name__ == '__main__':
+    x = MyList('spam')
+    print(x)
+    print(x[2])
+    print(x[1:])
+    print(x + ['eggs'])
+    print(x * 3)
+    x.append('a')
+    x.sort()
+    for c in x: print(c, end=' ')
+    
+class MySubList(MyList):
+    calls = 0
+    
+    def __init__(self, start):
+        self.counts = 0
+        MyList.__init__(self, start)
+    
+    def __add__(self, other):
+        MySubList.calls += 1
+        self.counts += 1
+        return MyList.__add__(self, other)
+
+    def stats(self):
+        return self.calls, self.counts
+    
+if __name__ == '__main__':
+    x = MySubList('spam')
+    y = MySubList('foo')
+    print(x[2])
+    print(x[1:])
+    print(x + ['eggs'])
+    print(x + ['toast'])
+    print(y + ['bar'])
+
